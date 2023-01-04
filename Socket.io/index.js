2389aa77b4cb02c92ec168f387e51645011614e4
leomarker <85751407+leomarker@users.noreply.github.com>
2023-01-04 16:41:33 +0300

    Brodcasting message with socket.io

---
+++ Socket.io/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+// io.on("connection", (socket) => {
+//   socket.on("chat message", (msg) => {
+//     console.log("message: " + msg);
+//   });
+// });
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("server runing at port 8080");
+});
