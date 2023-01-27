ce450a78599ef21c7623de229b8b80f9536f1bf9
Natnael Assefa <natnael3370@gmail.com>
2023-01-27 20:00:54 +0300

    server starter and gitignore template

--- Http server/app.js
+++ Http server/app.js
@@ -1,1 +1,11 @@
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("This is nodejs server");
+});
+
+let PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
