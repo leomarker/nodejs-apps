7339b4dbbee7f9283d168240ac12b88e1384b4de
Natnael Assefa <natnael3370@gmail.com>
2023-01-28 03:21:26 +0300

    adding a JSON server

--- Http server/app.js
+++ Http server/app.js
@@ -1,9 +1,33 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const JSONdata = JSON.stringify([
+  -128610405,
+  "bent",
+  [
+    {
+      meal: -2007318308.6798844,
+      pack: "window",
+      cheese: 1074790244,
+      apart: 1650846736,
+      facing: 577623722,
+      half: -548687929,
+    },
+    false,
+    "younger",
+    1669862118.6488533,
+    -1647484755.2707276,
+    "smoke",
+  ],
+  true,
+  407392355.508039,
+]);
+const JSONResponse = (req, res) => {
+  res.setHeader("Content-type", "application/json");
   res.writeHead(200);
-  res.end("This is nodejs server");
-});
+  res.end(JSONdata);
+};
+
+const server = http.createServer(JSONResponse);
 
 let PORT = 8080;
 server.listen(PORT, () => {
