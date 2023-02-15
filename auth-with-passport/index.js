769e67f24ffdc59c041381292045bcffb71ec812
Nati <natnael3370@gmail.com>
2023-02-15 17:47:25 +0300

    auth with passport

--- auth-with-passport/index.js
+++ auth-with-passport/index.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("login");
