def2a319a5d4ab62b38bdf7c064e315999057ad7
Nati <natnael3370@gmail.com>
2023-03-06 16:21:40 +0300

    parse a req body

--- Http server/index.js
+++ Http server/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const { parse } = require("querystring");
 const PORT = 3000;
 
 const server = http.createServer();
@@ -7,10 +8,43 @@ const server = http.createServer();
 const GETHandler = (req, res) => {
   if (req.url === "/" || req.url === "/home") {
     res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("you are on the home page");
+    res.end();
+  } else if (req.url === "/login") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("this is the login page");
+    res.end(`
+        <!doctype html>
+        <html>
+        <body>
+            <form action="/login" method="post">
+                <input type="email" name="email" /><br />
+                <input type="text" name="password" /><br />
+                <button>LOGIN</button>
+            </form>
+        </body>
+        </html>
+    `);
+  } else if (req.url === "/add") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("A value was added");
+    res.end();
   }
 };
 
-const POSTHandler = (req, res) => {};
+const POSTHandler = (req, res) => {
+  if (req.url === "/login") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      console.log(parse(body));
+    });
+
+    res.end();
+  }
+};
 
 const DELETEHandler = (req, res) => {};
 server.on("request", (req, res) => {
@@ -25,8 +59,20 @@ server.on("request", (req, res) => {
       DELETEHandler(req, res);
       break;
   }
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("asdfsa");
 });
 
 server.listen(8080);
+
+// res.end(`
+// <!doctype html>
+// <html>
+// <body>
+//     <form action="/" method="post">
+//         <input type="text" name="fname" /><br />
+//         <input type="number" name="age" /><br />
+//         <input type="file" name="photo" /><br />
+//         <button>Save</button>
+//     </form>
+// </body>
+// </html>
+// `);
