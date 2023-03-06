27cf2439335d6bb571683b5a713813adb72f21af
Nati <natnael3370@gmail.com>
2023-03-06 15:40:56 +0300

    adding a switch statment

--- Http server/index.js
+++ Http server/index.js
@@ -4,16 +4,26 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const GETHandler = (req, res) => {
+  if (req.url === "/" || req.url === "/home") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+  }
+};
+
+const POSTHandler = (req, res) => {};
+
+const DELETEHandler = (req, res) => {};
 server.on("request", (req, res) => {
-  if (req.method == "GET") {
-    if (req.url == "/" || req.url == "home") {
-    }
-    if (req.url == "/login") {
-    }
-    if (req.url == "/logout") {
-    }
-    if (req.url == "/add") {
-    }
+  switch (req.method) {
+    case "GET":
+      GETHandler(req, res);
+      break;
+    case "POST":
+      POSTHandler(req, res);
+      break;
+    case "DELETE":
+      DELETEHandler(req, res);
+      break;
   }
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("asdfsa");
