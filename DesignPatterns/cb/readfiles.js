30f9931f48f7bb5871f6e8b62a0241ca6a15549f
leomarker <natnael3370@gmail.com>
2023-10-25 18:45:03 +0300

    cb patterns in JS

---
+++ DesignPatterns/cb/readfiles.js
@@ -0,0 +1,37 @@
+import { readFile } from "fs";
+
+const cache = new Map();
+
+function ReadFile(fileName, cb) {
+  if (cache.has(fileName)) {
+    cb(cache.get(fileName));
+  } else {
+    readFile(fileName, "utf-8", (err, data) => {
+      cache.set(fileName, data);
+      return cb(data);
+    });
+  }
+}
+
+function createFileReader(fileName) {
+  const listeners = [];
+  ReadFile(fileName, (value) => {
+    listeners.forEach((listener) => listener(value));
+  });
+
+  return {
+    onDataReady: (listener) => listeners.push(listener),
+  };
+}
+
+const reader1 = createFileReader("data.txt");
+
+reader1.onDataReady((data) => {
+  console.log(`First data call:${data}`);
+
+  const reader2 = createFileReader("data.txt");
+
+  reader2.onDataReady((data) => {
+    console.log(`second data call:${data}`);
+  });
+});
