cd5f8f6a16ea5fa00cec85db5b571c861716c4b1
leomarker <natnael3370@gmail.com>
2023-10-26 14:44:03 +0300

    News letter subscription with observer pattern

--- DesignPatterns/observer/observerPatern.js
+++ DesignPatterns/observer/observerPatern.js
@@ -4,22 +4,63 @@
 
 */
 
-class NewsLatter {
+class NewsLetter {
   constructor() {
     this.subscribers = [];
+    this.newsLetter = {};
   }
 
-  addSubscriber(subscriber) {}
+  addSubscriber(subscriber) {
+    const hasAlreadySubscribed = this.subscribers.includes(subscriber);
 
-  removeSubscriber(subscriber) {}
+    if (hasAlreadySubscribed) {
+      return console.log("has already subscribed to this NewsLetter");
+    }
+    console.log(
+      `Dear ${subscriber.name} have been added to the NewsLetter Subscription`
+    );
+    this.subscribers.push(subscriber);
+  }
 
-  notify() {}
+  removeSubscriber(subscriber) {
+    const newsLetterSubscriber = this.subscribers.indexOf(subscriber);
 
-  newNewsLatter() {}
+    if (newsLetterSubscriber === -1) {
+      return console.log(
+        "Hmmmm it dons't look like you are subscribe to this NewsLetter"
+      );
+    }
+
+    this.subscribers.splice(newsLetterSubscriber, 1);
+    console.log("ooh! we are sorry to see you go!!");
+  }
+
+  notify() {
+    for (const subscriber of this.subscribers) {
+      subscriber.update(this.newsLetter);
+    }
+  }
+
+  composeNewLetter(date, title, body) {
+    this.newsLetter = {
+      date: date,
+      title: title,
+      body: body,
+    };
+
+    console.log("New letter is ready to go out");
+    this.notify();
+  }
 }
 
 class Subscriber {
-  constructor() {}
+  constructor(name) {
+    this.name = name;
+  }
 
-  update() {}
+  update(newsLetter) {
+    console.log(
+      `hey i am ${this.name} and i  have received the new news letter ${newsLetter} `
+    );
+  }
 }
