eb18e73899eb78e6c60972bb83d7ddc7523095fc
leomarker <natnael3370@gmail.com>
2023-11-26 16:03:42 +0300

    Concurrently running tasks with iteration pattern

---
+++ exerciese/callback-parallel-execution-flow/test.js
@@ -0,0 +1,31 @@
+function makeRandomTasks(name) {
+  return (cb) => {
+    console.log(`${name} started`);
+
+    setTimeout(() => {
+      console.log(`${name} has been completed`);
+      cb();
+    }, Math.random() * 1000);
+  };
+}
+
+tasks = [
+  makeRandomTasks("Task 1"),
+  makeRandomTasks("Task 2"),
+  makeRandomTasks("Task 3"),
+  makeRandomTasks("Task 4"),
+];
+
+let completed = 0;
+
+tasks.forEach((task) =>
+  task(() => {
+    if (++completed === tasks.length) {
+      finish();
+    }
+  })
+);
+
+function finish() {
+  console.log("All tasks has been completed ");
+}
