9bd6cbebae73234fc8c35b45c3a40bdbdc7e2d29
leomarker <natnael3370@gmail.com>
2023-12-01 10:09:53 +0300

    pass notes form the cli and append the on one destination file

--- exerciese/fileConcatenation/fileConcat-cli.js
+++ exerciese/fileConcatenation/fileConcat-cli.js
@@ -1,17 +1,5 @@
-import { fileConcatenation } from "./fileConcat.js";
+import { fileConcat } from "./fileConcat.js";
 
 console.log(process.argv.slice(2));
 
-fileConcatenation(
-  process.argv[2],
-  (err, filename) => {
-    if (err) {
-      console.log(err);
-    }
-
-    console.log(
-      `All you note have been concatenated and are now under the file name:${filename}`
-    );
-  },
-  ...process.argv.slice(3)
-);
+fileConcat(process.argv[2], ...process.argv.slice(3));
--- exerciese/fileConcatenation/fileConcat.js
+++ exerciese/fileConcatenation/fileConcat.js
@@ -1,59 +1,35 @@
 import fs from "fs";
 
-export const fileConcatenation = (dist, cb, ...notes) => {
-  let index = 0;
+let index = 0;
 
-  console.log(notes[index]);
-  next(notes[index], dist, (err) => {
+export function fileConcat(dist, ...notes) {
+  console.log(notes.length);
+  if (index === notes.length) {
+    return console.log("DONE");
+  }
+  copyNote(dist, notes[index], (err) => {
     if (err) {
       console.log(err);
-      cb(err);
     }
-
-    if (index === notes.length) {
-      cb(dist);
-    }
-
-    next(notes[++index], dist);
+    ++index;
+    fileConcat(dist, ...notes);
   });
-};
+}
 
-function next(file, dist, cb) {
-  readFileContent(file, (err, data) => {
+function copyNote(dist, note, cb) {
+  fs.readFile(note, "utf-8", (err, data) => {
     if (err) {
       console.log(err);
     }
 
     console.log(data);
 
-    writeFileContentToDist(dist, data, (err) => {
+    fs.appendFile(dist, data, "utf-8", (err) => {
       if (err) {
         console.log(err);
-        cb(err);
       }
 
       cb();
     });
-  });
-}
-
-function readFileContent(file, cb) {
-  fs.readFile(file, "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("YOUR not has be read");
-    cb(data);
-  });
-}
-
-function writeFileContentToDist(dist, data, cb) {
-  fs.writeFile(dist, data, "utf-8", (err) => {
-    if (err) {
-      console.log(err);
-      cb(err);
-    }
-
-    console.log(`New not has been written to ${dist}`);
   });
 }
