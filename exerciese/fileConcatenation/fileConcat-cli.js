158f68074556f390b8fd437059594280e91d1016
leomarker <natnael3370@gmail.com>
2023-12-01 10:57:04 +0300

    Different implementation for the not concatenation

--- exerciese/fileConcatenation/fileConcat-cli.js
+++ exerciese/fileConcatenation/fileConcat-cli.js
@@ -1,5 +1,5 @@
-import { fileConcat } from "./fileConcat.js";
+import { fileConcat, concatFiles } from "./fileConcat.js";
 
 console.log(process.argv.slice(2));
 
-fileConcat(process.argv[2], ...process.argv.slice(3));
+concatFiles(process.argv[2], ...process.argv.slice(3));
--- exerciese/fileConcatenation/fileConcat.js
+++ exerciese/fileConcatenation/fileConcat.js
@@ -33,3 +33,32 @@ function copyNote(dist, note, cb) {
     });
   });
 }
+
+export function concatFiles(dist, ...notes) {
+  let index = 0;
+  let content = "";
+
+  readNotes(notes, index, content, (err, note) => {
+    fs.writeFile(dist, note, "utf-8", (err) => {
+      if (err) console.log(err);
+
+      console.log("ALL not have been read");
+    });
+  });
+}
+
+function readNotes(notes, index, content, cb) {
+  console.log(index);
+  console.log(notes.length);
+
+  if (index === notes.length) {
+    console.log("true");
+    return cb(null, content);
+  }
+
+  fs.readFile(notes[index], "utf-8", (err, data) => {
+    if (err) return cb(err);
+    console.log(data);
+    readNotes(notes, ++index, content.concat(data, "\n"), cb);
+  });
+}
