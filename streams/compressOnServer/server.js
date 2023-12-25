75a7b6e74db93cf8e116c479dadd3d2121489a4b
leomarker <natnael3370@gmail.com>
2023-12-25 11:47:43 +0300

    server to accept connections and compress when file is received

---
+++ streams/compressOnServer/server.js
@@ -0,0 +1,25 @@
+import { createWriteStream } from "fs";
+import { createServer } from "http";
+import { basename, join } from "path";
+import { createGunzip } from "zlib";
+
+const server = createServer((req, res) => {
+  const filename = basename(req.headers["x-filename"]);
+  const destFilename = join("received_files", destFilename);
+
+  console.log(`File request received: ${filename}`);
+
+  req
+    .pipe(createGunzip())
+    .pipe(createWriteStream())
+    .on("finish", () => {
+      res.writeHead(201, { "Content-Type": "text/plain" });
+      res.end("Ok");
+
+      console.log(`File Saved : ${destFilename}`);
+    });
+});
+
+server.listen("8080", () => {
+  console.log("Server listing at port: 8080");
+});
