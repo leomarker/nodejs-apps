26af85413213b88011c9116610c1b68b51a2a3ab
leomarker <natnael3370@gmail.com>
2023-12-25 11:17:17 +0300

    ask user to enter path to file

--- streams/compresFiles/index.js
+++ streams/compresFiles/index.js
@@ -3,19 +3,31 @@ import { gzip } from "node:zlib";
 import { promisify } from "node:util";
 
 const gzipPromise = promisify(gzip);
-const filename = process.argv[2];
-
-// while (filename === null) {
-//   console.log("No file provided");
-// }
+let filename = process.argv[2];
 
 async function main() {
+  while (!filename) {
+    console.log("No file provided. Please enter a file path:");
+    const input = await getUserInput();
+    filename = input.trim();
+  }
+
   const data = await readFile(filename);
   const gzippedData = await gzipPromise(data);
 
   await writeFile(`${filename}.gz`, gzippedData);
 
   console.log("File compressed");
+
+  process.exit(0);
+}
+
+async function getUserInput() {
+  return new Promise((resolve) => {
+    process.stdin.once("data", (data) => {
+      resolve(data.toString().trim());
+    });
+  });
 }
 
 main();
