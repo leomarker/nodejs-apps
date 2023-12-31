807a1d1ee5226b67202fec87a5f56458732e5fe1
Natnael Assefa <natnael3370@gmail.com>
2023-12-31 19:39:20 +0300

    readable stream form the stdin

---
+++ streams/Readable-stream/random-stream.js
@@ -0,0 +1,21 @@
+import { Readable } from "stream";
+import Chance from "chance";
+
+let chance = new Chance();
+
+export class RandomStream extends Readable {
+  constructor(options) {
+    super(options);
+    this.emittedBytes = 0;
+  }
+
+  _read(size) {
+    const chunk = chance.string({ length: size });
+    this.push(chunk, "utf-8");
+    this.emittedBytes += chunk.length;
+
+    if (chance.bool({ likelihood: 5 })) {
+      this.pause(null);
+    }
+  }
+}
